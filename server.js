07aa55ce43252b9e635bca753150ae423564e3d5
JabariD <dennispayton15@gmail.com>
2020-09-09 06:27:31 -0500

    Change port

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("server running...")
 });
 
